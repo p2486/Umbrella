fa5a5db4bb1ad8137a9c43bd231f8d3706592be2
is191325 <is191325@fhstp.ac.at>
2021-11-27 18:16:44 +0100

    http response

--- junit5/src/main/java/at/ac/fhstp/App.java
+++ junit5/src/main/java/at/ac/fhstp/App.java
@@ -1,8 +1,10 @@
 package at.ac.fhstp;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
+
+        CoinMarket cm = new CoinMarket();
+        System.out.print(cm.request());
+
     }
-}   
+}
---
+++ junit5/src/main/java/at/ac/fhstp/CoinMarket.java
@@ -0,0 +1,53 @@
+package at.ac.fhstp;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpClient;
+
+public class CoinMarket {
+
+    private String APIKey = "380ca9de-b801-4a31-8fd7-c2b5b6051c29";
+
+    public String request() {
+
+        // curl -H "X-CMC_PRO_API_KEY: b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c" -H "Accept:
+        // application/json" -d "start=1&limit=5000&convert=USD" -G
+        // https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest
+
+        try {
+
+            HttpRequest req = HttpRequest
+
+                    .newBuilder()
+                    .uri(new URI("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest"))
+                    .setHeader("X-CMC_PRO_API_KEY", APIKey) // add
+                    .setHeader("Accept", "application/json") // add
+                    // request
+                    // header
+
+                    .GET()
+                    .build();
+
+            HttpResponse<String> res = HttpClient
+                    .newBuilder()
+                    .build()
+                    .send(req, HttpResponse.BodyHandlers.ofString());
+
+            // Hier wäre es schöner wenn gleich ein Stream retour kommt.
+            // Siehe
+            // https://www.programcreek.com/java-api-examples/?api=java.net.http.HttpResponse
+            // -> Beispiel 15
+            System.out.print("HTTP Status: " + res.toString());
+            return res.body().toString();
+        } catch (URISyntaxException | IOException | InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+
+}
