352ad5a60947defb6ed61922076a59486c88da02
is191325 <is191325@fhstp.ac.at>
2021-11-28 09:20:40 +0100

    asdasd

--- junit5/src/main/java/at/ac/fhstp/App.java
+++ junit5/src/main/java/at/ac/fhstp/App.java
@@ -3,7 +3,7 @@ package at.ac.fhstp;
 public class App {
     public static void main(String[] args) {
 
-        CoinMarket cm = new CoinMarket();
+        CryptoReader_CoinMarket cm = new CryptoReader_CoinMarket();
         System.out.print(cm.request());
 
     }
